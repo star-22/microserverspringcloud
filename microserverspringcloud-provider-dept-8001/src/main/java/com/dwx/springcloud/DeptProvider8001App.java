292f132d7413bf43f5bd55c9fad34d37ee2447fa
dengwenxing <2528435919@qq.com>
2019-09-28 02:14:41 +0800

    dwx第一次提交

--- microserverspringcloud-provider-dept-8001/src/main/java/com/dwx/springcloud/DeptProvider8001App.java
+++ microserverspringcloud-provider-dept-8001/src/main/java/com/dwx/springcloud/DeptProvider8001App.java
@@ -8,5 +8,6 @@ public class DeptProvider8001App {
     public static void main(String[] args) {
         SpringApplication.run(DeptProvider8001App.class,args);
         System.out.println("springboot启动成功");
+
     }
 }
